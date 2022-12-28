597b89f579817abe05b9150acf0cee4db7cd91bb
khanhnd <khanh.nttu0204@gmail.com>
2022-12-28 15:31:52 +0700

    deploy-code

--- example/src/App.js
+++ example/src/App.js
@@ -4,7 +4,7 @@ import { ExampleComponent } from 'text-icon-xlab'
 import 'text-icon-xlab/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <ExampleComponent text="Create React Library Example 😄" icon="😄"/>
 }
 
 export default App
